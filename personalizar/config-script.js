6d9c61abc01b9e0a74c839a6ccb8bf4fd513127a
dan lima <danieldelimamendes500@gmail.com>
2024-08-16 05:39:11 -0300

    .

--- personalizar/config-script.js
+++ personalizar/config-script.js
@@ -6,8 +6,6 @@ document.getElementById('add-input-condition').addEventListener('click', functio
     addMessageEntry(true);
 });
 
-<<<<<<< Updated upstream
-=======
 document.getElementById('add-email-message').addEventListener('click', function() {
     addEmailMessageEntry();
 });
@@ -16,10 +14,12 @@ document.getElementById('add-response-button').addEventListener('click', functio
     addResponseButtonEntry();
 });
 
->>>>>>> Stashed changes
+document.getElementById('add-redirect-link').addEventListener('click', function () {
+    addRedirectLinkEntry();
+});
+
 function addMessageEntry(isConditional) {
     const messagesContainer = document.getElementById('messages-container');
-    
     const messageEntry = document.createElement('div');
     messageEntry.className = 'message-entry';
     
@@ -29,26 +29,19 @@ function addMessageEntry(isConditional) {
         const conditionInput = document.createElement('input');
         conditionInput.type = 'text';
         conditionInput.placeholder = 'Exemplo: 1';
-<<<<<<< Updated upstream
-        conditionInput.className = 'condition-input';  // Adicionei uma classe para identificar inputs de condição
-=======
         conditionInput.className = 'condition-input';  
         conditionInput.dataset.type = "condicao";  
->>>>>>> Stashed changes
         messageEntry.appendChild(conditionLabel);
         messageEntry.appendChild(conditionInput);
     }
     
     const messageLabel = document.createElement('label');
     messageLabel.textContent = 'Mensagem:';
-    
     const messageInput = document.createElement('textarea');
     messageInput.placeholder = 'Digite sua mensagem aqui...';
     messageInput.rows = 2;
-    
     const imageLabel = document.createElement('label');
     imageLabel.textContent = 'Imagem (opcional):';
-    
     const imageInput = document.createElement('input');
     imageInput.type = 'file';
     imageInput.accept = 'image/*';
@@ -57,35 +50,27 @@ function addMessageEntry(isConditional) {
     messageEntry.appendChild(messageInput);
     messageEntry.appendChild(imageLabel);
     messageEntry.appendChild(imageInput);
-    
     messagesContainer.appendChild(messageEntry);
 }
 
 function addEmailMessageEntry() {
     const messagesContainer = document.getElementById('messages-container');
-
     const messageEntry = document.createElement('div');
     messageEntry.className = 'message-entry';
-
     const messageLabel = document.createElement('label');
     messageLabel.textContent = 'Mensagem:';
-
     const messageInput = document.createElement('textarea');
     messageInput.placeholder = 'Digite a mensagem solicitando o email (ex: Digite seu email: )';
     messageInput.rows = 1;
-
     const emailInputLabel = document.createElement('label');
     emailInputLabel.textContent = 'Label do Campo Email (opcional):'; 
-
     const emailInput = document.createElement('input');
     emailInput.type = 'text';
     emailInput.placeholder = 'Label do Campo (opcional - deixe em branco para o padrão)';
     emailInput.className = 'email-label'; 
     emailInput.dataset.type = 'email'; 
-
     const emailValidationLabel = document.createElement('label');
     emailValidationLabel.textContent = 'Validação do Email:'; 
-
     const emailValidationMessage = document.createElement('textarea');
     emailValidationMessage.placeholder = 'Mensagem a ser exibida em caso de erro (ex: Email inválido. Por favor, tente novamente.)';
     emailValidationMessage.rows = 1;
@@ -97,28 +82,22 @@ function addEmailMessageEntry() {
     messageEntry.appendChild(emailInput);
     messageEntry.appendChild(emailValidationLabel);
     messageEntry.appendChild(emailValidationMessage);
-
     messagesContainer.appendChild(messageEntry);
 }
 
 function addResponseButtonEntry() {
     const messagesContainer = document.getElementById('messages-container');
-
     const responseEntry = document.createElement('div');
     responseEntry.className = 'message-entry';
-
     const buttonTextLabel = document.createElement('label');
     buttonTextLabel.textContent = 'Texto do Botão:';
-
     const buttonInput = document.createElement('input');
     buttonInput.type = 'text'; 
     buttonInput.className = 'button-label'; 
     buttonInput.placeholder = 'Texto do Botão'; 
     buttonInput.dataset.type = 'button'; 
-
     const responseLabel = document.createElement('label');
     responseLabel.textContent = 'Resposta Associada:';
-
     const responseText = document.createElement('textarea');
     responseText.placeholder = 'Digite a resposta associada ao botão...';
     responseText.rows = 2;
@@ -129,23 +108,44 @@ function addResponseButtonEntry() {
     responseEntry.appendChild(buttonInput);
     responseEntry.appendChild(responseLabel);
     responseEntry.appendChild(responseText);
-
     messagesContainer.appendChild(responseEntry);
+}
+
+function addRedirectLinkEntry() {
+  const messagesContainer = document.getElementById('messages-container');
+  const redirectEntry = document.createElement('div');
+  redirectEntry.className = 'message-entry';
+  const redirectTextLabel = document.createElement('label');
+  redirectTextLabel.textContent = 'Texto do Link:'; 
+  const redirectTextInput = document.createElement('input');
+  redirectTextInput.type = 'text';
+  redirectTextInput.className = 'redirect-text';
+  redirectTextInput.placeholder = 'Ex: Clique aqui para saber mais'; 
+  redirectTextInput.dataset.type = 'redirect'; 
+  const redirectLinkLabel = document.createElement('label');
+  redirectLinkLabel.textContent = 'URL de Redirecionamento:'; 
+  const redirectLinkInput = document.createElement('input');
+  redirectLinkInput.type = 'url';
+  redirectLinkInput.className = 'redirect-link'; 
+  redirectLinkInput.placeholder = 'Ex: https://www.exemplo.com';
+  redirectLinkInput.dataset.type = 'redirect'; 
+
+  redirectEntry.appendChild(redirectTextLabel);
+  redirectEntry.appendChild(redirectTextInput);
+  redirectEntry.appendChild(redirectLinkLabel);
+  redirectEntry.appendChild(redirectLinkInput);
+  messagesContainer.appendChild(redirectEntry);
 }
 
 document.getElementById('typebot-form').addEventListener('submit', function (e) {
     e.preventDefault();
-    
     const typebotName = document.getElementById('typebot-name').value;
+    const avatarURL = document.getElementById('typebot-avatar').value;
+    const displayName = document.getElementById('typebot-display-name').value;
+    const status = document.getElementById('typebot-status').value;
     const messageEntries = document.querySelectorAll('.message-entry');
-    
+
     const messages = Array.from(messageEntries).map(entry => {
-<<<<<<< Updated upstream
-        return {
-            text: entry.querySelector('textarea').value,
-            image: entry.querySelector('input[type="file"]').files[0],
-            condition: entry.querySelector('.condition-input') ? entry.querySelector('.condition-input').value : null
-=======
         const text = entry.querySelector('textarea') ? entry.querySelector('textarea').value : null;
         const imageInput = entry.querySelector('input[type="file"]');
         const image = imageInput ? imageInput.files[0] : null;
@@ -154,6 +154,8 @@ document.getElementById('typebot-form').addEventListener('submit', function (e) 
         const validationMessage = entry.querySelector('.email-validation') ? entry.querySelector('.email-validation').value : null;
         const buttonLabel = entry.querySelector('.button-label') ? entry.querySelector('.button-label').value : null;
         const buttonResponse = entry.querySelector('.response-text') ? entry.querySelector('.response-text').value : null;
+        const redirectText = entry.querySelector('.redirect-text') ? entry.querySelector('.redirect-text').value : null;
+        const redirectLink = entry.querySelector('.redirect-link') ? entry.querySelector('.redirect-link').value : null;
         const type = entry.querySelector('[data-type]') ? entry.querySelector('[data-type]').dataset.type : null;
 
         return {
@@ -164,28 +166,27 @@ document.getElementById('typebot-form').addEventListener('submit', function (e) 
             emailLabel: emailLabel,
             buttonLabel: buttonLabel,
             buttonResponse: buttonResponse,
+            redirectText: redirectText, 
+            redirectLink: redirectLink,
             type: type
->>>>>>> Stashed changes
         };
     });
-    
+
     const formData = new FormData();
     formData.append('name', typebotName);
+    formData.append('avatar', avatarURL);
+    formData.append('displayName', displayName);
+    formData.append('status', status);
+    
     messages.forEach((message, index) => {
-<<<<<<< Updated upstream
-        formData.append(`messages[${index}][text]`, message.text);
-=======
         formData.append(`messages[${index}][text]`, message.text || '');
         formData.append(`messages[${index}][email_label]`, message.emailLabel || '');
->>>>>>> Stashed changes
         if (message.image) {
             formData.append(`messages[${index}][image]`, message.image);
         }
         if (message.condition) {
             formData.append(`messages[${index}][condition]`, message.condition || '');
         }
-<<<<<<< Updated upstream
-=======
         if (message.validationMessage) {
             formData.append(`messages[${index}][validationMessage]`, message.validationMessage || '');
         }
@@ -198,9 +199,14 @@ document.getElementById('typebot-form').addEventListener('submit', function (e) 
         if (message.buttonResponse) {
             formData.append(`messages[${index}][buttonResponse]`, message.buttonResponse || '');
         }
->>>>>>> Stashed changes
+        if (message.redirectText) {
+            formData.append(`messages[${index}][redirectText]`, message.redirectText || '');
+        }
+        if (message.redirectLink) {
+            formData.append(`messages[${index}][redirectLink]`, message.redirectLink || '');
+        }
     });
-    
+
     fetch('http://localhost:5000/create_typebot', {
         method: 'POST',
         body: formData
--- script.js
+++ script.js
@@ -1,247 +1,255 @@
 let messageIndex = 0;
 let messages = [];
 let typebotId;
+let typebotAvatar;
 
 document.addEventListener("DOMContentLoaded", function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    typebotId = urlParams.get('id');
+  const urlParams = new URLSearchParams(window.location.search);
+  typebotId = urlParams.get('id');
 
-    if (typebotId) {
-        fetchMessages(typebotId);
-    } else {
-        alert('ID do Typebot não encontrado na URL.');
-    }
+  if (typebotId) {
+    fetchMessages(typebotId);
+  } else {
+    alert('ID do Typebot não encontrado na URL.');
+  }
 });
 
 function fetchMessages(typebotId) {
-    fetch(`http://localhost:5000/get_messages/${typebotId}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.messages && data.messages.length > 0) {
-<<<<<<< Updated upstream
-=======
-                typebotAvatar = data.avatar;
-                document.getElementById('avatar').src = data.avatar;
-                document.getElementById('display-name').textContent = data.displayName;
-                document.getElementById('status').textContent = data.status || 'Online';
->>>>>>> Stashed changes
-                messages = data.messages;
-                showNextMessage();
-            } else {
-                console.error('Nenhuma mensagem encontrada para o Typebot.');
-            }
-        })
-        .catch(error => {
-            console.error('Erro ao buscar mensagens:', error);
-        });
-}
-
-<<<<<<< Updated upstream
-function showNextMessage() {
-    if (messageIndex < messages.length) {
-        const currentMessage = messages[messageIndex];
-=======
-function displayMessage(message) {
-    const typingBubble = document.createElement('div');
-    typingBubble.className = 'bubble-typing';
-    typingBubble.innerHTML = '<span>.</span><span>.</span><span>.</span>';
-    const typingElement = document.createElement('div');
-    typingElement.className = 'message typebot-host-bubble typing';
-    typingElement.appendChild(typingBubble);
-    document.querySelector('.chat-box').appendChild(typingElement);
->>>>>>> Stashed changes
-
-        setTimeout(() => {
-            // Verificar se a mensagem atual tem uma condição e deve mostrar um input
-            if (currentMessage.condition) {
-                showInputPrompt(currentMessage.condition);
-            } else {
-                displayMessage(currentMessage);
-            }
-
-<<<<<<< Updated upstream
-            scrollToBottom();
-        }, 2000);
-    }
-=======
-    setTimeout(() => {
-        typingElement.remove();
-
-        const messageElement = document.createElement('div');
-        messageElement.className = 'message typebot-host-bubble';
-
-        const avatar = document.createElement('div');
-        avatar.className = 'avatar';
-    
-        const img = document.createElement('img');
-        img.src = typebotAvatar; 
-    
-        img.onload = function() {
-            avatar.appendChild(img);
-        };
-        img.onerror = function() {
-            console.error("Erro ao carregar o avatar.");
-        };
-    
-        messageElement.appendChild(avatar);
-
-        const messageContent = document.createElement('div');
-        messageContent.className = 'bubble-content';
-        
-        if (message.image_path) {
-            const img = document.createElement("img");
-            img.src = message.image_path;
-            img.alt = "Imagem";
-            messageContent.appendChild(img);
-        } else if (message.button_label) {
-            // Criação do botão clicável
-            const button = document.createElement('button');
-            // Define a classe do botão
-            button.className = 'response-button'; // Classe para estilização
-            // Define o texto do botão
-            button.textContent = message.button_label;
-
-            // Adiciona o botão ao container do chat como uma mensagem do usuário 
-            const userMessage = document.createElement('div');
-            userMessage.className = 'message typebot-user-bubble';
-            userMessage.appendChild(button);
-            document.querySelector('.chat-box').appendChild(userMessage); 
-
-            // Adiciona um event listener ao botão
-            button.addEventListener('click', () => {
-                // Exibe a resposta associada ao botão na tela do chat
-                displayUserMessage(message.button_label); // Envia o button_label como se o usuário tivesse escrito
-                // Exibe a mensagem do bot com a resposta
-                displayMessage({text: message.button_response}); // Exibe o button_response como resposta do bot
-
-                // Remove o botão após o clique
-                button.remove(); 
-
-            });
-        } else {
-            messageContent.textContent = message.text;
-        }
-        
-        messageElement.appendChild(messageContent);
-        document.querySelector('.chat-box').appendChild(messageElement);
-
-        scrollToBottom();
-
-        if (message.type === 'email' || message.type === 'condicao') {
-            setTimeout(() => {
-                if (message.type === 'email') {
-                    showEmailPrompt(message);
-                } else if (message.type === 'condicao') {
-                    showInputPrompt(message.condition);
-                }
-            }, 2000);
-        }
-    }, 1000);
->>>>>>> Stashed changes
-}
-
-function showInputPrompt(condition) {
-    const inputContainer = document.createElement('div');
-    inputContainer.className = 'message typebot-guest-bubble input-container';
-
-    const inputField = document.createElement('input');
-    inputField.type = 'text';
-    inputField.placeholder = 'Digite sua resposta...';
-
-    const submitButton = document.createElement('button');
-    submitButton.textContent = 'Enviar';
-    submitButton.addEventListener('click', () => {
-        const userInput = inputField.value.trim();
-        if (userInput) {
-            displayUserMessage(userInput);
-
-            inputContainer.remove();
-
-            handleUserInput(userInput, condition);
-        } else {
-            alert('Por favor, insira uma resposta.');
-        }
-    });
-
-    inputContainer.appendChild(inputField);
-    inputContainer.appendChild(submitButton);
-    document.querySelector('.chat-box').appendChild(inputContainer);
-
-    scrollToBottom();
-}
-
-function handleUserInput(userInput, condition) {
-    const matchingMessage = messages.find(message => message.condition === condition);
-    if (matchingMessage && userInput === condition) {
-        displayMessage(matchingMessage);
-    } else {
+  fetch(`http://localhost:5000/get_messages/${typebotId}`)
+    .then(response => response.json())
+    .then(data => {
+      if (data.messages && data.messages.length > 0) {
+        typebotAvatar = data.avatar;
+        document.getElementById('avatar').src = data.avatar;
+        document.getElementById('display-name').textContent = data.displayName;
+        document.getElementById('status').textContent = data.status || 'Online';
+        messages = data.messages;
         showNextMessage();
-    }
+      } else {
+        console.error('Nenhuma mensagem encontrada para o Typebot.');
+      }
+    })
+    .catch(error => {
+      console.error('Erro ao buscar mensagens:', error);
+    });
 }
 
-<<<<<<< Updated upstream
-function displayMessage(message) {
-    const typingBubble = document.createElement('div');
-    typingBubble.className = 'bubble-typing';
+function displayResponseButton(message) {
+  const button = document.createElement("button");
+  button.className = "response-button";
+  button.textContent = message.button_label;
 
-    if (message.image_path) {
-        const img = document.createElement("img");
-        img.src = message.image_path;
-        img.alt = "Imagem";
-        typingBubble.appendChild(img);
-    } else {
-        typingBubble.textContent = message.text;
-    }
+  const userMessage = document.createElement("div");
+  userMessage.className = "message typebot-user-bubble";
+  userMessage.appendChild(button);
+  document.querySelector(".chat-box").appendChild(userMessage);
+
+  button.addEventListener("click", () => {
+    displayUserMessage(message.button_label);
+    button.remove(); 
+    displayMessage({ text: message.button_response });
+    messageIndex++;
+    showNextMessage(); 
+  });
+
+  scrollToBottom();
+}
+
+function displayMessage(message) {
+  if (message.button_label) {
+    displayResponseButton(message);
+    return;
+  }
+
+  const typingBubble = document.createElement('div');
+  typingBubble.className = 'bubble-typing';
+  typingBubble.innerHTML = '<span>.</span><span>.</span><span>.</span>';
+  const typingElement = document.createElement('div');
+  typingElement.className = 'message typebot-host-bubble typing';
+  typingElement.appendChild(typingBubble);
+  document.querySelector('.chat-box').appendChild(typingElement);
+
+  scrollToBottom();
+
+  setTimeout(() => {
+    typingElement.remove();
 
     const messageElement = document.createElement('div');
     messageElement.className = 'message typebot-host-bubble';
-    messageElement.appendChild(typingBubble);
+
+    const avatar = document.createElement('div');
+    avatar.className = 'avatar';
+
+    const img = document.createElement('img');
+    img.src = typebotAvatar;
+
+    img.onload = function () {
+      avatar.appendChild(img);
+    };
+    img.onerror = function () {
+      console.error("Erro ao carregar o avatar.");
+    };
+
+    messageElement.appendChild(avatar);
+
+    const messageContent = document.createElement('div');
+    messageContent.className = 'bubble-content';
+
+    if (message.image_path) {
+      const img = document.createElement("img");
+      img.src = message.image_path;
+      img.alt = "Imagem";
+      messageContent.appendChild(img);
+    } else {
+      messageContent.textContent = message.text;
+    }
+
+    messageElement.appendChild(messageContent);
     document.querySelector('.chat-box').appendChild(messageElement);
-=======
-function showEmailPrompt(message) {
-    const emailContainer = document.createElement('div');
-    emailContainer.className = 'message typebot-guest-bubble input-container'; 
-
-    const emailInput = document.createElement('input');
-    emailInput.type = 'email';
-    emailInput.className = 'styled-input'; 
-    emailInput.placeholder = message.email_label || 'Seu Email';
-    emailInput.required = true;
-
-    const submitButton = document.createElement('button');
-    submitButton.className = 'styled-button'; 
-    submitButton.textContent = 'Enviar';
-    submitButton.addEventListener('click', () => {
-        const userEmail = emailInput.value.trim();
-
-        if (isValidEmail(userEmail)) {
-            displayUserMessage(userEmail);
-
-            emailContainer.remove();
-
-            handleEmailInput(userEmail, message);
-        } else {
-            alert(message.validationMessage || 'Por favor, digite um email válido.');
-            emailInput.focus();
-        }
-    });
-
-    emailContainer.appendChild(emailInput);
-    emailContainer.appendChild(submitButton);
-    document.querySelector('.chat-box').appendChild(emailContainer);
 
     scrollToBottom();
+
+    if (message.type === 'email' || message.type === 'condicao') {
+      setTimeout(() => {
+        if (message.type === 'email') {
+          showEmailPrompt(message);
+        } else if (message.type === 'condicao') {
+          showInputPrompt(message.condition);
+        }
+      }, 2000);
+    }
+  }, 1000);
+}
+
+function showInputPrompt(condition) {
+  const inputContainer = document.createElement('div');
+  inputContainer.className = 'message typebot-guest-bubble input-container';
+
+  const inputField = document.createElement('input');
+  inputField.type = 'text';
+  inputField.placeholder = 'Digite sua resposta...';
+  inputField.required = true;
+
+  const submitButton = document.createElement('button');
+  submitButton.textContent = 'Enviar';
+  submitButton.addEventListener('click', () => {
+    const userInput = inputField.value.trim();
+    if (userInput) {
+      displayUserMessage(userInput);
+      inputContainer.remove();
+      handleUserInput(userInput, condition);
+    } else {
+      alert('Por favor, insira uma resposta.');
+    }
+  });
+
+  inputContainer.appendChild(inputField);
+  inputContainer.appendChild(submitButton);
+  document.querySelector('.chat-box').appendChild(inputContainer);
+  scrollToBottom();
+}
+
+function showNextMessage() {
+  if (messageIndex < messages.length) {
+    const currentMessage = messages[messageIndex];
+    displayMessage(currentMessage);
+    if (currentMessage.type !== 'email' && currentMessage.type !== 'condicao') {
+      messageIndex++;
+      setTimeout(showNextMessage, 2000);
+    }
+  } else {
+    console.log("Fim da conversa do typebot.");
+  }
+}
+
+function showEmailPrompt(message) {
+  const emailContainer = document.createElement('div');
+  emailContainer.className = 'message typebot-guest-bubble input-container';
+
+  const emailInput = document.createElement('input');
+  emailInput.type = 'email';
+  emailInput.className = 'styled-input';
+  emailInput.placeholder = message.email_label || 'Seu Email';
+  emailInput.required = true;
+
+  const submitButton = document.createElement('button');
+  submitButton.className = 'styled-button';
+  submitButton.textContent = 'Enviar';
+  submitButton.addEventListener('click', () => {
+    const userEmail = emailInput.value.trim();
+    if (isValidEmail(userEmail)) {
+      displayUserMessage(userEmail);
+      emailContainer.remove();
+      handleEmailInput(userEmail, message);
+    } else {
+      alert(message.validationMessage || 'Por favor, digite um email válido.');
+      emailInput.focus();
+    }
+  });
+
+  emailContainer.appendChild(emailInput);
+  emailContainer.appendChild(submitButton);
+  document.querySelector('.chat-box').appendChild(emailContainer);
+
+  scrollToBottom();
 }
 
 function handleEmailInput(userEmail, message) {
-    console.log("Email recebido:", userEmail);
->>>>>>> Stashed changes
+  console.log("Email recebido:", userEmail);
+  messageIndex++;
+  showNextMessage();
+}
 
-    messageIndex++;
-    showNextMessage();
+function handleUserInput(userInput, condition) {
+  if (userInput === condition) {
+    const matchingMessage = messages.find(message => message.condition === condition);
+    if (matchingMessage) {
+      setTimeout(() => {
+        displayMessage(matchingMessage);
+        showNextMessage();
+      }, 2000);
+    } else {
+      console.error("Mensagem para condição não encontrada.");
+    }
+  } else {
+    displayUserMessage(userInput);
+  }
+}
+
+function displayUserMessage(message) {
+  const userMessage = document.createElement('div');
+  userMessage.className = 'message typebot-user-bubble';
+  const messageText = document.createElement('div');
+  messageText.className = 'bubble-content';
+  messageText.textContent = message;
+  userMessage.appendChild(messageText);
+  document.querySelector('.chat-box').appendChild(userMessage);
+  scrollToBottom();
+}
+
+function sendMessage() {
+  const userInput = document.getElementById('user-input').value.trim();
+
+  if (userInput) {
+    const userMessage = document.createElement('div');
+    userMessage.className = 'message typebot-guest-bubble';
+    const messageText = document.createElement('div');
+    messageText.className = 'bubble-content';
+    messageText.textContent = userInput;
+
+    userMessage.appendChild(messageText);
+    document.querySelector('.chat-box').appendChild(userMessage);
+    document.getElementById('user-input').value = '';
+    scrollToBottom();
+  }
+}
+
+function isValidEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
 }
 
 function scrollToBottom() {
-    const chatBox = document.querySelector('.chat-box');
-    chatBox.scrollTop = chatBox.scrollHeight;
+  const chatBox = document.querySelector('.chat-box');
+  chatBox.scrollTop = chatBox.scrollHeight;
 }
